744b817843f79b79fbdc111b9e3882e53fdefd9d
sahilbasu5101 <sahilbasu5101@gmail.com>
2026-03-31 23:59:36 +0530

    changees

--- javascript/string.js
+++ javascript/string.js
@@ -1,4 +1,4 @@
-let name = "sahil"
+// let name = "sahil"
 console.log(name.length);
 console.log(name.toUpperCase());
 console.log(name.toLowerCase());
@@ -6,15 +6,38 @@ console.log(name.slice(2,4));
 console.log(name.replace("hil", "bil"));
 let friend = "Naman";
 console.log(name.concat(" is a friend of ", friend));
+let Name = "sahil basu";
+let val = "mr. ";
+for(let i of Name){
+  val += i;
+}
+console.log(val);
+let name = "lolo";
+name[0] = "A";
+console.log(name);//string are immputable
 
 // console.log(name.splice(2,3));
+
 /*
+STRING METHODS:- 
+
 String length
+
 String charAt()
+let text = "HELLO WORLD";
+let char = text.charAt(0);
+
 String charCodeAt()
+let text = "HELLO WORLD";
+let char = text.charCodeAt(0);
+
 String codePointAt()
+
 String concat()
+console.log(name.concat(" is a friend of ", friend));
+
 String at()
+
 String [ ]
 String slice()
 String substring()
